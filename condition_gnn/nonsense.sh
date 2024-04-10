6bbda8d4de90277644638d234bba4b0ac11744e0
caltdreamer <145941818+caltdreamer@users.noreply.github.com>
2024-04-10 16:58:32 -0500

    Add files via upload

---
+++ condition_gnn/nonsense.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=array
+#SBATCH --output=experiments/logs/array_er_%A_%a.out
+#SBATCH --error=experiments/logs/array_er_%A_%a.err
+#SBATCH --array=1-1
+#SBATCH --time=1:00:00
+#SBATCH --partition=caslake
+#SBATCH --mem=10G
+#SBATCH --account=pi-cdonnat
+
+# Print the task id.
+echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+echo "My SLURM_ARRAY_JOB_ID: " $SLURM_ARRAY_JOB_ID
+
+
+# Add lines here to run your computations
+
+
+job_id=$SLURM_ARRAY_JOB_ID
+
+
+result_file="${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}"
+echo "result file is ${result_file}"
+cd $SCRATCH/$USER/$GNN/$condition_gnn
+
+module load gsl
+module load gcc
+module load python/anaconda-2021.05
+python3 main.py  --model GCN --dataset Cora_ML_CF --device cpu --alpha 0.05 --num_runs 1  --seed 0 
+\
+
