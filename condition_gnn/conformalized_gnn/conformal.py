c0674e5d1a7f26ed1526692eae982a5eec43b6cc
caltdreamer <145941818+caltdreamer@users.noreply.github.com>
2024-11-04 15:21:01 -0600

    Update conformal.py

--- condition_gnn/conformalized_gnn/conformal.py
+++ condition_gnn/conformalized_gnn/conformal.py
@@ -211,8 +211,8 @@ def run_conformal_classification(pred, data, n, alpha, score = 'aps',
                 cal_features = current_feature[idx]
                 test_features = current_feature[~idx]
                 test_labels = val_labels
-                
-                if calib_eval & identify==2:
+                print(identify)
+                if identify==2:
                         variables_to_save = {'cal_features': cal_features, 'cal_labels': cal_labels,'test_features':test_features,'test_labels':test_labels,'idx':idx,'num_features':data.num_features,'prediction':smx,'num_classes':smx.shape[1]}
                         serialized_file_name = 'run_condition_variables_CFGNN_2.pkl'
                         serialized_file_path = './' + serialized_file_name  # This specifies the current directory
